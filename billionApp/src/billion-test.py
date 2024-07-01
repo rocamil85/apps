2b122af08025002646265c7554cea94170fa91e3
rocamil85 <rocamil85@gmail.com>
2024-07-01 00:18:47 +0000

    Initial commit

---
+++ billionApp/src/billion-test.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from google.cloud import storage
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def process_data():
+    file_name = 'World_Billionaire_2024.csv'
+    file_path = f'gs://bucket_billion/{file_name}'
+
+    # Leer el archivo directamente desde GCS
+    df = pd.read_csv(file_path)
+    df_cleaned = df.dropna()
+
+    # Guardar los datos procesados en un nuevo archivo CSV
+    processed_file_name = f'processed_{file_name}'
+    processed_file_path = f'gs://bucket_billion_process/{processed_file_name}'
+    df_cleaned.to_csv(processed_file_path, index=False)
+
+    print(f"procesado documento: {processed_file_name}")
+    return 'Data processed and saved successfully!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
